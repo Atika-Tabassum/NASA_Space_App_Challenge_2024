1165dabd7b703a4c13a91ef223423eaac5fc95ac
Anika Morshed <anikamorshed4434@gmail.com>
2024-10-05 11:10:33 +0600

    merge

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import FetchDataComponent from './apis/FetchDataComponent';
 import Study from './study/Study';
 import DailyGoal from './dailygoal/DailyGoal';
 import Awareness from './awareness/Awareness';
+import FrontPage from './main/FrontPage';
+import Chat from "./chatComponent/Chat";
 function App() {
   return (
     <Router>
@@ -25,6 +27,8 @@ function App() {
           <Route path="/sustainify/memory" element={<MemoryGame />} />
           <Route path="/sustainify/dailygoal" element={<DailyGoal />} />
           <Route path="/sustainify/awareness" element={<Awareness />} />
+          <Route path="/" element={<FrontPage />} />
+          <Route path="/sustainify/chat" element={<Chat />} />
         </Routes>
       </Fragment>
     </Router>
